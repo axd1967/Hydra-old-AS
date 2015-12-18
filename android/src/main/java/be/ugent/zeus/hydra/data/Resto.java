ac8e65b231e95c09a33831724ef6deca78b6c172
alex <alexander.duytschaever@gmail.com>
2015-12-18 10:29:20 +0100

    import Hydra 1.1.0 (eclipse) into AS 1.5

---
+++ android/src/main/java/be/ugent/zeus/hydra/data/Resto.java
@@ -0,0 +1,16 @@
+package be.ugent.zeus.hydra.data;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Thomas Meire
+ */
+public class Resto implements Serializable {
+
+    public String name;
+    public String address;
+    public Double longitude;
+    public Double latitude;
+    public boolean resto;
+}
